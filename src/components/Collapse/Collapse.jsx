89b09e38d53ff6166ad3cf9f177739072e677ec9
theRoseOfThePhoenix <sandrinebecaert@gmail.com>
2024-02-29 19:31:53 +0100

    collapse scss

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -6,7 +6,7 @@ function Collapse({ title, content }) {
     <div className="collapse">
       <div className="collapse_titre" onClick={() => setIsOpen(false)}>
         {title}
-        <img className="collapse_arrow" src={Arrow} alt="flèche du bas" />
+        <img className="collapse_arrow" src={Arrow} alt="flèche du haut" />
       </div>
       <div className="collapse_content">{content}</div>
     </div>
@@ -14,7 +14,7 @@ function Collapse({ title, content }) {
     <div className="collapse">
       <div className="collapse_titre" onClick={() => setIsOpen(true)}>
         {title}
-        <img className="collapse_arrow" src={Arrow} alt="flèche du bas" />
+        <img className="collapse_arrow--up" src={Arrow} alt="flèche du bas" />
       </div>
     </div>
   );
