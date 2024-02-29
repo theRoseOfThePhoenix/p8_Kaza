79b3295fc7655193b05c8d1963da22e44879e77f
theRoseOfThePhoenix <sandrinebecaert@gmail.com>
2024-02-29 18:48:59 +0100

    collapse avec state

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,6 +1,23 @@
-import React from "react";
-
-function Collapse() {
-  return <div>ipubg</div>;
+import React, { useState } from "react";
+import Arrow from "../../asset/down_arrow.png";
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return isOpen ? (
+    <div className="collapse">
+      <div className="collapse_titre" onClick={() => setIsOpen(false)}>
+        {title}
+        <img className="collapse_arrow" src={Arrow} alt="flèche du bas" />
+      </div>
+      <div className="collapse_content">{content}</div>
+    </div>
+  ) : (
+    <div className="collapse">
+      <div className="collapse_titre" onClick={() => setIsOpen(true)}>
+        {title}
+        <img className="collapse_arrow" src={Arrow} alt="flèche du bas" />
+      </div>
+    </div>
+  );
 }
+
 export default Collapse;
